8e9e5299d7291017452f2323a57f3572635af242
Fudgecake13 <alifajr35@gmail.com>
2026-07-06 16:07:25 +0500

    Implement basic player turn logic in script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("Javascript is working!");
+const cells = document.querySelectorAll(".cell");
+
+let currentPlayer = "X";
+
+cells.forEach(cell => {
+cell.addEventListener("click", handleClick);
+});
+
+function handleClick() {
+this.textContent = currentPlayer;
+currentPlayer = currentPlayer === "X" ? "❤️" : "X";
+}
